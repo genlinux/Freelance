0a682f94b353572f8c1de8c30d3aab29e0f9c97a
Gen.lostwarrior <genlinuxworks@gmail.com>
2009-12-13 18:23:13 +0530

    Diff between proc and lamda

---
+++ Ruby/Metaprogramming/lamdavsproc.rb
@@ -0,0 +1,26 @@
+#def method
+#  puts "at top"
+#  
+#  puts "at end"
+#end
+#
+#puts "before call"
+#
+#method
+#
+#puts "after call"
+
+#With Proc
+def proc_return_method
+  puts "at top"
+  
+  pr=Proc.new{return}
+  pr.call
+  
+  puts "at end"
+end
+puts "before call"
+
+proc_return_method
+
+puts "after call"
