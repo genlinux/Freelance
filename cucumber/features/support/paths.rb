29323d0a01c9cab0e478b5d9d6986326e39c16b3
Gen.lostwarrior <genlinuxworks@gmail.com>
2009-12-18 19:47:33 +0530

    steps updated path added with urls, routes configured

--- cucumber/features/support/paths.rb
+++ cucumber/features/support/paths.rb
@@ -11,6 +11,8 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
     
+  when /the list of users/
+    '/users'
     # Add more mappings here.
     # Here is a more fancy example:
     #
