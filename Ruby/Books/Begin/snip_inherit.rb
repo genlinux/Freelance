7db9e05aea8f0ebdae39501685d88cefe08c401c
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-06 15:42:12 +0530

    Simple basic example for inheritance

---
+++ Ruby/Books/Begin/snip_inherit.rb
@@ -0,0 +1,16 @@
+#This code has been taken from Peter Coopers Apress Beginning ruby from Novice Professional
+#This will explains about basic inheritance
+#Inheriting Pets
+#Normal method
+class Cat
+  attr_accessor :name, :age, :gender, :color
+end
+
+class Dog
+  attr_accessor :name, :age, :gender, :color
+end
+
+class Snake
+  attr_accessor :name, :age, :gender, :color
+end
+
