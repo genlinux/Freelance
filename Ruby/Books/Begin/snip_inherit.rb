efe2dc3feaedfa8aa2a2478991a0708023b160bf
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-06 16:58:06 +0530

    Simple basic example for inheritance with methods  and attributes

--- Ruby/Books/Begin/snip_inherit.rb
+++ Ruby/Books/Begin/snip_inherit.rb
@@ -14,3 +14,27 @@ class Snake
   attr_accessor :name, :age, :gender, :color
 end
 
+#Explanation
+#Creating the classes in this way would miss out on one of the best features of
+#oriented programming :inheritance
+#Inheritance allows different classes to relate to one another and group concepts by their similarities
+
+#Lets try with the clear code
+class Pet
+  attr_accessor :name, :age, :gender, :color
+end
+
+class Cat < Pet
+end
+
+class Dog < Pet
+  def bark
+  puts "Woof!"
+  end
+end
+
+class Snake < Pet
+#attributes that aren’t relevant to every animal
+  attr_accessor :length
+end
+
