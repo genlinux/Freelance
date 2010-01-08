21468844a64406639e48f74922e1ac04d468678d
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-08 15:39:41 +0530

    Simple average program with input output

--- Ruby/Algorithms/Al1.rb
+++ Ruby/Algorithms/Al1.rb
@@ -1,7 +1,10 @@
-#
+# Author : Genlinux
 # This file contains the Ruby code from 
 # "Data Structures and Algorithms
 # with Object-Oriented Design Patterns in Ruby"
+# A Simple input output program
+puts "Please type the number"
+n=gets().to_i
 def sum(n)
     result = 0
     i = 1
@@ -11,3 +14,4 @@ def sum(n)
     end
     return result
 end
+puts sum(n)
