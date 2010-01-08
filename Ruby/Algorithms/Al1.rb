db825f5a5c48df3fc70b9de523652dd158f53900
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-08 15:07:43 +0530

    Basic algorithm sample one

---
+++ Ruby/Algorithms/Al1.rb
@@ -0,0 +1,13 @@
+#
+# This file contains the Ruby code from 
+# "Data Structures and Algorithms
+# with Object-Oriented Design Patterns in Ruby"
+def sum(n)
+    result = 0
+    i = 1
+    while i <= n
+        result += i
+        i += 1
+    end
+    return result
+end
