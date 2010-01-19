277d710d3914f4430db91d2bcfe9c4ec6c77b22d
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-19 20:37:25 +0530

    Program changed

--- Ruby/Screex/rubyist/lib/rubyist.rb
+++ Ruby/Screex/rubyist/lib/rubyist.rb
@@ -2,11 +2,8 @@
 class Temperature
   
   puts "Please type the temperature for conversion"
-  celsius = gets().to_i
-  
-  def self.conversion(celsius)
-    fahrenheit = (celsius * 9/5) + 32
+  def conversion(celsius)
+    fahrenheit = (celsius.to_i * 9/5) + 32
     puts "The result is : " + "#{fahrenheit}"
   end
-  puts conversion(celsius)
 end
