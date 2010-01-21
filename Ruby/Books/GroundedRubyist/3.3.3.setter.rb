2781a1a30e4fd71e1cd9842909babc16f19f45fb
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-01-21 16:37:58 +0530

    setter method example

---
+++ Ruby/Books/GroundedRubyist/3.3.3.setter.rb
@@ -0,0 +1,12 @@
+class Ticket
+  def price=(amount)
+    if(amount * 100).to_i == amount * 100
+    @price=amount
+    else
+    puts "The price seems to be malformed"
+    end
+  end
+  def price
+    @price
+  end
+end
