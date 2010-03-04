bd5791e652ce3be781c993b64ba371d978e6787a
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-03-04 16:31:23 +0530

    migration file for picture model

--- Rails/assoc/db/migrate/20100304104913_create_pictures.rb
+++ Rails/assoc/db/migrate/20100304104913_create_pictures.rb
@@ -2,9 +2,7 @@ class CreatePictures < ActiveRecord::Migration
   def self.up
     create_table :pictures do |t|
       t.string :name
-      t.integer :imageable_id
-      t.string :imageable_type
-
+      t.references :imageable, :polymorphic => true 
       t.timestamps
     end
   end
