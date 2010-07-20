b71fc6241948e55168476ef04753f13261fa2c19
Gen.lostwarrior <genlinuxworks@gmail.com>
2010-07-20 06:13:13 +0530

    new columns added to user model

--- Rails/webt/db/schema.rb
+++ Rails/webt/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100717124935) do
+ActiveRecord::Schema.define(:version => 20100719175632) do
 
   create_table "article_contents", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(:version => 20100717124935) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "last_login"
+    t.datetime "remember_token_expires_at"
+    t.string   "remember_token"
   end
 
 end
